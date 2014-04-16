66e70ed4f723041b4c579d65c1c3ab7b5f3a8cf9
megamattman <matthew7.hall@live.uwe.ac.uk>
2014-04-16 13:17:43 +0100

    added driver test program

---
+++ dTest.c
@@ -0,0 +1,12 @@
+//C program for opening and writing/reading to the driver
+
+#include <stdio.h>
+
+int main (void)
+{
+  FILE *driverFp;
+  driverFp = fopen("/dev/pio0", "r");
+  printf("OPENING DRIVER!!\n");
+  fclose(driverFp);
+  printf("Close DRIVERS!!\n");
+}
